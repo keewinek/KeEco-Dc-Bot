2e710c560c79849b260447dce6d22dca03e25db7
keewinek <keewinek@gmail.com>
2024-02-03 12:52:09 +0100

    Added test 'ping' command!

--- app.py
+++ app.py
@@ -1,1 +1,23 @@
-print("Hello world!")
+import os
+
+import discord
+from dotenv import load_dotenv
+from discord.ext import commands
+from discord import app_commands
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client(command_prefix='$', intents=discord.Intents.all())
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+@bot.tree.command(name="ping", description="Replies with Pong!")
+async def ping(interaction: discord.Interaction):
+    await interaction.response.send_message("Pong!")
+    await bot.tree.sync()
+
+
+client.run(TOKEN)
