3a357c96b3d04ca57bd4471a127a53e4984f9a7d
keewinek <keewinek@gmail.com>
2024-02-03 13:10:15 +0100

    Added test 'ping' command!

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ bot = commands.Bot(command_prefix='$', intents=discord.Intents.all())
 async def on_ready():
     print(f'{bot.user} has connected to Discord!')
 
+    try:
+        synced = await bot.tree.sync()
+        print(f"Synced {len(synced)} command(s)")
+    except Exception as e:
+        print(e)
+
 @bot.tree.command(name="ping", description="Replies with Pong! (check if the bot is online)")
 async def ping(interaction: discord.Interaction):
     await interaction.response.send_message("Pong!")
